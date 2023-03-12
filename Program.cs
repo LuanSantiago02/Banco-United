4f337dfc0fc640ca4a27bd91dfcd960547bbe98d
Luan Santiago <111893167+LuanSantiago02@users.noreply.github.com>
2023-03-12 14:52:03 -0300

    United Bank
    Programa para testes, porem muito bem feito!

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Banco_United.Classes;
+
+namespace Banco_United 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Layout.TelaPrincipal();
+        }
+    }
+}
